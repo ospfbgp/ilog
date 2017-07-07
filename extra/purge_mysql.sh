21c0fcb97aff60a1aceaa55f9a8d9c4de88f6afa
ospfbgp <danielcoxman@gmail.com>
2017-07-06 23:58:57 -0400

    Update purge_mysql.sh

--- extra/purge_mysql.sh
+++ extra/purge_mysql.sh
@@ -3,8 +3,8 @@
 # needs to run ass cron daily
 SQL="DELETE FROM syslog WHERE datetime < DATE_SUB(CURDATE(),INTERVAL 5:DAY)"
 
-MYSQL_USER=""
-MYSQL_PASS=""
+MYSQL_USER="ilog"
+MYSQL_PASS="ilogpassword"
 MYSQL_DB="ilog"
 
 echo $SQL | /usr/bin/mysql --user=$MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB

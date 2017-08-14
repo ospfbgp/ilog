2764eb089d4b172e24e6ebafc47de8f8f8759dfc
ospfbgp <danielcoxman@gmail.com>
2017-08-14 14:49:05 -0400

    Update purge_mysql.sh

--- extra/purge_mysql.sh
+++ extra/purge_mysql.sh
@@ -10,6 +10,5 @@ MYSQL_USER="ilog"
 MYSQL_PASS="ilogpassword"
 MYSQL_DB="ilog"
 
-DELETE FROM syslog WHERE seq NOT IN ( SELECT seq FROM ( SELECT seq FROM syslog ORDER BY seq DESC LIMIT 500000 ) ilog);
 echo $SQL_syslog | /usr/bin/mysql --user=$MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB
 echo $SQL_ignition_catId_10 | /usr/bin/mysql --user=$MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB

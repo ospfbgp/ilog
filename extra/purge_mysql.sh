55201d1815e681d5df19e97047873e956b60248c
ospfbgp <danielcoxman@gmail.com>
2017-08-14 14:47:13 -0400

    Update purge_mysql.sh

--- extra/purge_mysql.sh
+++ extra/purge_mysql.sh
@@ -2,11 +2,14 @@
 # This will remove all records older than 7 days
 # needs to run ass cron daily
 SQL_syslog="DELETE FROM syslog WHERE datetime < NOW() - INTERVAL 7 DAY"
+#SQL syslog="DELETE FROM syslog WHERE seq NOT IN ( SELECT seq FROM ( SELECT seq FROM syslog ORDER BY seq DESC LIMIT 500000 ) ilog);"
 SQL_ignition_catId_10="DELETE FROM ignition_catId_10 WHERE datetime < NOW() - INTERVAL 7 DAY"
+#SQL_ignition_catId_10="DELETE FROM SQL_ignition_catId_10 WHERE seq NOT IN ( SELECT seq FROM ( SELECT seq FROM SQL_ignition_catId_10 ORDER BY seq DESC LIMIT 100000 ) ilog);"
 
 MYSQL_USER="ilog"
 MYSQL_PASS="ilogpassword"
 MYSQL_DB="ilog"
 
+DELETE FROM syslog WHERE seq NOT IN ( SELECT seq FROM ( SELECT seq FROM syslog ORDER BY seq DESC LIMIT 500000 ) ilog);
 echo $SQL_syslog | /usr/bin/mysql --user=$MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB
 echo $SQL_ignition_catId_10 | /usr/bin/mysql --user=$MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB

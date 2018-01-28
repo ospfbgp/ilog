fb7ed4d5afec10230359356cdb14dee053a32329
ospfbgp <danielcoxman@gmail.com>
2018-01-28 09:42:01 -0500

    Update ilog_nac.sql

--- extra/ilog_nac.sql
+++ extra/ilog_nac.sql
@@ -1,3 +1,7 @@
+USE ilog;
+
+CREATE TABLE `nac` (
+    `datetime` datetime DEFAULT NULL,
     `macAddress` varchar(32) DEFAULT NULL,
     `ipAddress` varchar(32) DEFAULT NULL,
     `username` varchar(100) DEFAULT NULL,
